ef8429d9d7d3aa5786ac5fedaf2eb8af3f2c180d
Vinícius Mendes Lopes <69206@PROWAY.TREINA>
2020-11-01 17:10:33 -0300

    2.1 - Implementação dos metodos de conexao com o mysql

--- src/main/java/br/com/agenda/data/ConexaoMySql.java
+++ src/main/java/br/com/agenda/data/ConexaoMySql.java
@@ -1,25 +1,60 @@
 package br.com.agenda.data;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConexaoMySql implements Conexao{
+	
+	private Connection connection = null;
+	
+	private static final String USERNAME = "root";
+	
+	private static final String PASSWORD = "";
+	
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agenda?useTimezone=true&serverTimezone=UTC";
+	
+	public ConexaoMySql() throws SQLException, ClassNotFoundException{
+		Class.forName("com.mysqljdbc.Driver");
+		this.connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+		this.connection.setAutoCommit(false);
+	}
 
-	@Override
+	
 	public Connection getConnection() {
-		return null;
+		return this.connection;
 	}
 
-	@Override
+	
 	public void close() {
+		if(this.connection != null) {
+			try {
+				this.connection.close();
+			} catch (SQLException ex) {
+				Logger.getLogger(ConexaoMySql.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
 	}
 
-	@Override
-	public void commit() throws SQLException {		
+
+	public void commit() throws SQLException {
+		this.connection.commit();
+		this.close();
 	}
 
-	@Override
-	public void rollback() {		
+
+	public void rollback() {	
+		if(this.connection != null) {
+			try {
+				this.connection.rollback();
+			} catch (SQLException ex) {
+				Logger.getLogger(ConexaoMySql.class.getName()).log(Level.SEVERE, null, ex);
+			} finally {
+				this.close();
+			}
+		}
 	}
 
 }
